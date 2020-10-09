aa7a030213fb44c88b2706b2dd66ad0670d99997
GLukeM-H <glukemh@gmail.com>
2020-10-08 23:47:12 -0700

    Updates

---
+++ precache-manifest.fbdcbdb43471f28e6706383acedbd663.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5386516b9a15fbbabbfc6dd8d94da41",
+    "url": "/juke/index.html"
+  },
+  {
+    "revision": "7dced105a4139535566b",
+    "url": "/juke/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13fa6dfb248a8b9bd76a",
+    "url": "/juke/static/js/2.fedd85b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/juke/static/js/2.fedd85b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dced105a4139535566b",
+    "url": "/juke/static/js/main.a143b239.chunk.js"
+  },
+  {
+    "revision": "3c2c0031ca10bc00b093",
+    "url": "/juke/static/js/runtime-main.e855b510.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/juke/static/media/logo.5d5d9eef.svg"
+  }
+]);
