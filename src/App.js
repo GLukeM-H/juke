847778fae5c0c127eb76ce5b0edf501b683227aa
Jacob Mimms <jacobmimms@gmail.com>
2020-10-10 18:36:37 -0400

    plz
    this is my brach

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React PLZ
         </a>
       </header>
     </div>
